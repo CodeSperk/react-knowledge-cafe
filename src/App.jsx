aed94545201b4e885237a4ccdb94314ba3ac2761
Mahbubur Rahman <inbx.mahbub@gmail.com>
2024-03-13 19:17:14 +0600

    added header and created fake data

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import './App.css'
+import Header from './components/header/Header'
 
 function App() {
 
   return (
-    <>
-     hhh
-    </>
+    <div className="max-w-7xl p-3 md:p-6 lg:p-12 mx-auto">
+     <Header></Header>
+
+
+
+    </div>
   )
 }
 
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,14 @@
+
+const Header = () => {
+  return (
+    <div className="mt-12 flex items-center justify-between">
+      <h1 className="text-4xl font-bold">Knowledge Cafe</h1>
+      <div>
+        <img src="./images/profile.png" alt="" style={{width:"60px", height:"60px"}}/>
+      </div>
+
+    </div>
+  );
+};
+
+export default Header;
